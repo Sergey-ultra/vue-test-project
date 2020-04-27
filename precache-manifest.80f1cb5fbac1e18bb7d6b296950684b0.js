9227445f74a2cd8f11a3fc2529250b3752f8be08
Lenovo <maasa@list.ru>
2020-04-28 00:28:29 +0400

    deploy

---
+++ precache-manifest.80f1cb5fbac1e18bb7d6b296950684b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d06216d4012a60c3040f",
+    "url": "/vue-test-project/css/app.9f384b35.css"
+  },
+  {
+    "revision": "59f844c057cf81754eb3",
+    "url": "/vue-test-project/css/chunk-vendors.40df3b9d.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/vue-test-project/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/vue-test-project/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/vue-test-project/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/vue-test-project/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "655c1ef3148aff75a1aa26277ad95322",
+    "url": "/vue-test-project/img/5.655c1ef3.gif"
+  },
+  {
+    "revision": "1e99ac96a24e83511b23428453fb1dac",
+    "url": "/vue-test-project/index.html"
+  },
+  {
+    "revision": "d06216d4012a60c3040f",
+    "url": "/vue-test-project/js/app.bf48c221.js"
+  },
+  {
+    "revision": "59f844c057cf81754eb3",
+    "url": "/vue-test-project/js/chunk-vendors.12c3da16.js"
+  },
+  {
+    "revision": "a19db3eafd039b633e973087eb113d25",
+    "url": "/vue-test-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-test-project/robots.txt"
+  }
+]);
